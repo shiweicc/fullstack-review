d75ac6a9b9df08e39f91db873a2e6bc288f2491a
Shiwei Chen <shiweichenus@gmail.com>
2022-05-30 23:30:07 -0500

    Fixed the bug in Search Component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,22 +8,23 @@ class Search extends React.Component {
     }
   }
 
-  onChange (e) {
+  // arrow function will not redefine 'this', doesn't need bind(this) anymore
+  onChange = (e) => {
     this.setState({
       term: e.target.value
     });
   }
 
-  search() {
+  search = () => {
     this.props.onSearch(this.state.term);
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
